ab3a3cce2ab85435ac36752d9e19c7a306a8473a
Cobapakai <khen.dedes10@gmail.com>
2021-10-05 21:16:30 +0700

    ambigu part 2

--- app/src/main/java/com/example/tugaskami/MainActivity.java
+++ app/src/main/java/com/example/tugaskami/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.tugaskami;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SensorManager sensorManager;
+    Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
     }
 }
