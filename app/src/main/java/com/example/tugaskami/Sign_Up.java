2102e8338aa55be9d1ba7043b19a5735dadec517
Cobapakai <khen.dedes10@gmail.com>
2021-10-05 16:52:06 +0700

    ambigu

--- app/src/main/java/com/example/tugaskami/Sign_Up.java
+++ app/src/main/java/com/example/tugaskami/Sign_Up.java
@@ -51,7 +51,7 @@ public class Sign_Up extends AppCompatActivity implements View.OnClickListener {
 
         }
 
-//        Toast.makeText(Sign_Up.this, "Selamat Anda Sudah Mendaftar, Silahkan Login Kembali", Toast.LENGTH_SHORT).show();
+//       Toast.makeText(Sign_Up.this, "Selamat Anda Sudah Mendaftar, Silahkan Login Kembali", Toast.LENGTH_SHORT).show();
 
     }
 
