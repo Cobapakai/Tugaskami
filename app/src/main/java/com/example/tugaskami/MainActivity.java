0faede9c6fc8f88e023468ee9ca192b9dfbb1505
Cobapakai <khen.dedes10@gmail.com>
2021-10-08 20:35:08 +0700

    kid commit

--- app/src/main/java/com/example/tugaskami/MainActivity.java
+++ app/src/main/java/com/example/tugaskami/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "MainActivity";
 
@@ -19,9 +22,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "onCreate: Initializing Sensor Services");
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registered accelerometer listener");
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d(TAG, "onSensorChanged: X:" + sensorEvent.values[0] + "Y:" + sensorEvent.values[1] + "Z:" + sensorEvent.values[2]);
 
     }
 }
--- app/src/main/java/com/example/tugaskami/Menu.java
+++ app/src/main/java/com/example/tugaskami/Menu.java
@@ -2,13 +2,63 @@ package com.example.tugaskami;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+    ImageView profil, pengaturan, map, pesan, history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        profil = findViewById(R.id.profil);
+        profil.setOnClickListener(this);
+
+        pengaturan = findViewById(R.id.pengaturan);
+        pengaturan.setOnClickListener(this);
+
+        map = findViewById(R.id.map);
+        map.setOnClickListener(this);
+
+        pesan = findViewById(R.id.pesan);
+        pesan.setOnClickListener(this);
+
+        history = findViewById(R.id.history);
+        history.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.profil:
+                Intent intent = new Intent(this, Me.class);
+                startActivity(intent);
+                break;
+
+            case R.id.pengaturan:
+                Intent pengaturan = new Intent(this, Setting.class);
+                startActivity(pengaturan);
+                break;
+
+            case R.id.map:
+                Intent map = new Intent(this, Me.class);
+                startActivity(map);
+                break;
+
+            case R.id.pesan:
+                Intent pesan = new Intent(this, Me.class);
+                startActivity(pesan);
+                break;
+
+            case R.id.history:
+                Intent history = new Intent(this, Me.class);
+                startActivity(history);
+                break;
+        }
+
     }
 }
