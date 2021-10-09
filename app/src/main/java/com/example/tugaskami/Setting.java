053524f59fb735c5b7826df03e2caab2bf852e49
Cobapakai <khen.dedes10@gmail.com>
2021-10-09 20:40:33 +0700

    kid commit 2

--- app/src/main/java/com/example/tugaskami/Setting.java
+++ app/src/main/java/com/example/tugaskami/Setting.java
@@ -2,13 +2,47 @@ package com.example.tugaskami;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class Setting extends AppCompatActivity {
+public class Setting extends AppCompatActivity implements View.OnClickListener {
+    ImageButton kembali;
+    TextView account, help, about, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        kembali = findViewById(R.id.kembali);
+        kembali.setOnClickListener(this);
+
+        account = findViewById(R.id.account);
+        account.setOnClickListener(this);
+
+        help = findViewById(R.id.help);
+        help.setOnClickListener(this);
+
+        about = findViewById(R.id.about);
+        about.setOnClickListener(this);
+
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.kembali:
+                Intent kembali = new Intent(this, Menu.class);
+                startActivity(kembali);
+                break;
+
+        }
+
+    }
+
 }
