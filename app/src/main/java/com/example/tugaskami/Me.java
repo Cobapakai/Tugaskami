f4cd2a7dd80acc5281b09a617928ba07a18ff0ca
Cobapakai <khen.dedes10@gmail.com>
2021-10-11 20:40:28 +0700

    tambah back

--- app/src/main/java/com/example/tugaskami/Me.java
+++ app/src/main/java/com/example/tugaskami/Me.java
@@ -2,14 +2,28 @@ package com.example.tugaskami;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class Me extends AppCompatActivity {
+public class Me extends AppCompatActivity implements View.OnClickListener {
+    ImageButton kembali;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me);
+
+        kembali = findViewById(R.id.kembali);
+        kembali.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.kembali) {
+            Intent kembali = new Intent(this, Menu.class);
+            startActivity(kembali);}
     }
 }
